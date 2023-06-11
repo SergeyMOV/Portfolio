c11e5dff5728a2d14dfe20caf7c45cfa494378ab
Sergey Movsesyan <sm877141@gmail.com>
2023-06-11 17:45:48 +0200

    fixed some Issues

--- index.js
+++
@@ -1,3 +0,0 @@
-const menubuttons = document.querySelector("nav>ul>li>a");
-const homebutton = document.querySelector(".homepagebutton")
-const buttonText = document.querySelector('[data-js="buttontext"]');
