2ace56b014de6ea4df0073a6219c82f66bf55c63
Sergey Movsesyan <sm877141@gmail.com>
2023-06-11 13:14:16 +0200

    second commit

--- ServiceWeb/index.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('[data-js="button"]');
-const buttonText = document.querySelector('[data-js="buttontext"]');
-button.addEventListener("mouseover",()=>{
-buttonText.style.color="black";
-})
-button.addEventListener("mouseout",()=>{
-	buttonText.style.color="white";
-	})
