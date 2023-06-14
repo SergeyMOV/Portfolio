5f31eea548657ba53c3e4bd4fb8260e0ebde4493
Sergey Movsesyan <sm877141@gmail.com>
2023-06-14 16:43:15 +0200

    Added some mobile responsivity

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const aboutButton = document.querySelector('[data-js="button"]');
 const homeButton = document.querySelector('[data-js="homebt"]');
+const x = window.matchMedia("(min-width: 420px)");
 
 if (history.scrollRestoration) {
     history.scrollRestoration = 'manual';
-} else {
+}
+else {
     window.onbeforeunload = function () {
         window.scrollTo(0, 0);
     }
 }
-
-aboutButton.onclick = function() { 
-	window.scrollBy({ top: 900, left: 0, behavior: "smooth" });
+aboutButton.onclick = function() {
+    if(x.matches){
+        window.scrollBy({ top: 1923, left: 0, behavior: "smooth" });
+    }
+    else{
+        window.scrollBy({ top: 900, left: 0, behavior: "smooth" });
+    }
 }
 homeButton.onclick = function(){
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
