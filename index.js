49ecbac45516401427dbcc371337f5c946c3f763
Sergey Movsesyan <sm877141@gmail.com>
2023-06-15 18:34:21 +0200

    Added some new Components

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const aboutButton = document.querySelector('[data-js="button"]');
+const aboutButton = document.querySelector('[data-js="Aboutbutton"]');
 const homeButton = document.querySelector('[data-js="homebt"]');
 const homelink = document.querySelector('[data-js="homelink"]');
 const x = window.matchMedia("(min-width: 420px)");
+const y = window.matchMedia("(orientation: portrait)");
 
 if (history.scrollRestoration) {
     history.scrollRestoration = 'manual';
@@ -12,8 +13,11 @@ else {
     }
 }
 aboutButton.onclick = function() {
-    if(x.matches){
+    if(x.matches && y.matches){
         window.scrollBy({ top: 2250, left: 0, behavior: "smooth" });
+        document.querySelector("li.About").style.border="1px solid white";
+        document.querySelector("li.About").style.borderRadius="8px 8px";
+        homeButton.style.border="none";
     }
     else{
         window.scrollBy({ top: 900, left: 0, behavior: "smooth" });
@@ -21,6 +25,10 @@ aboutButton.onclick = function() {
 }
 homeButton.onclick = function(){
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    if(x.matches && y.matches){
+    document.querySelector("li.About").style.border="none";
+    homeButton.style.border="1px solid white";
+}
 }
 homeButton.addEventListener("mouseover", ()=>{
 homelink.style.color="skyblue";
