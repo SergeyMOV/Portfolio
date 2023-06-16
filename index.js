2cff69ea099873267fba9759d6c63c612ea49745
Sergey M <sm877141@gmail.com>
2023-06-16 13:36:24 +0200

    added Responsivity for Large Screens

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const homelink = document.querySelector('[data-js="homelink"]');
 const header = document.querySelector('[data-js="Header"]');
 const x = window.matchMedia("(min-width: 420px)");
 const y = window.matchMedia("(orientation: portrait)");
+const s = window.matchMedia("(orientation: landscape)");
+const m = window.matchMedia("(min-width: 1280px)");
 if (history.scrollRestoration) {
     history.scrollRestoration = 'manual';
 }
@@ -18,6 +20,9 @@ aboutButton.onclick = function() {
         document.querySelector("li.About").style.border="1px solid white";
         document.querySelector("li.About").style.borderRadius="8px 8px";
         homeButton.style.border="none";
+    }
+    else if (s.matches && m.matches){
+        window.scrollBy({ top: 1100, left: 0, behavior: "smooth" });
     }
     else{
         window.scrollBy({ top: 900, left: 0, behavior: "smooth" });
