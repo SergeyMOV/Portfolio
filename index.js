87c7724c6b6bd3cac64b2cf41038381526e9e0b5
Sergey Movsesyan <sm877141@gmail.com>
2023-07-10 11:55:26 +0200

    fixed some Responsivity Issues

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ document.querySelector("li.About").style.borderRadius="8px 8px";
             document.querySelector("li.portf").style.borderRadius="8px 8px";
             document.querySelector("li.portf").style.boxShadow ="2px 5px 13px 5px #0985ba";
         }
-        else if(m.matches && c.matches && window.scrollY>=1800  || ml.matches && c.matches && window.scrollY>=1800){
+        else if(m.matches && c.matches && window.scrollY>=1800 || ml.matches && c.matches && window.scrollY>=1800){
             document.querySelector("li.About").style.border="none";
             document.querySelector("li.About").style.boxShadow ="none";
             document.querySelector("li.portf").style.border="1px solid";
